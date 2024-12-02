14bb59e0ca5b18eea88e0d6b79dc642a3f41f732
Weston Clark <117543803+westonclark@users.noreply.github.com>
2024-12-02 14:02:42 -0800

    Solve day 1

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -3,29 +3,110 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
+const (
+	inputFile = "input.txt"
+)
+
 func partOne(lines []string) int {
-	return 0
+	result := 0
+
+	leftValues := []int{}
+	rightValues := []int{}
+
+	for _, line := range lines {
+		columns := strings.Split(line, " ")
+
+		leftInt, err := strconv.Atoi(columns[0])
+		if err != nil {
+			fmt.Println("Error converting left collum to int", err)
+			return 0
+		}
+		rightInt, err := strconv.Atoi(columns[len(columns)-1])
+		if err != nil {
+			fmt.Println("Error converting right collum to int", err)
+			return 0
+		}
+
+		leftValues = append(leftValues, leftInt)
+		rightValues = append(rightValues, rightInt)
+	}
+
+	// calulate the difference between the two values
+	sort.Ints(leftValues)
+	sort.Ints(rightValues)
+
+	for i := 0; i < len(leftValues); i++ {
+		difference := 0
+		if leftValues[i] > rightValues[i] {
+			difference = leftValues[i] - rightValues[i]
+		} else {
+			difference = rightValues[i] - leftValues[i]
+		}
+
+		result += difference
+	}
+
+	return result
 }
 
 func partTwo(lines []string) int {
-	return 0
+	type valueCount struct {
+		leftCount  int
+		rightCount int
+	}
+
+	similarityScore := 0
+	values := map[int]valueCount{}
+
+	for _, line := range lines {
+		columns := strings.Split(line, " ")
+
+		leftInt, err := strconv.Atoi(columns[0])
+		if err != nil {
+			fmt.Println("Error converting left collum to int", err)
+			return 0
+		}
+		rightInt, err := strconv.Atoi(columns[len(columns)-1])
+		if err != nil {
+			fmt.Println("Error converting right collum to int", err)
+			return 0
+		}
+
+		// add left values to map
+		valueCounts := values[leftInt]
+		valueCounts.leftCount++
+		values[leftInt] = valueCounts
+
+		// add right values to map
+		valueCounts = values[rightInt]
+		valueCounts.rightCount++
+		values[rightInt] = valueCounts
+	}
+
+	// calulate the similarity score for each value in the map
+	for value, counts := range values {
+		if counts.leftCount > 0 {
+			similarityScore += value * counts.rightCount
+		}
+	}
+
+	return similarityScore
 }
 
 func main() {
-	data, err := os.ReadFile("input.txt")
+	data, err := os.ReadFile(inputFile)
 	if err != nil {
-		fmt.Println("Error reading input file", err)
-		return
+		fmt.Fprintf(os.Stderr, "Error reading input file: %v\n", err)
+		os.Exit(1)
 	}
 
-	lines := strings.Split(string(data), "\n")
-	for _, line := range lines {
-		fmt.Println(line)
-	}
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
 
-	fmt.Println("Part 1 result:", partOne(lines))
-	fmt.Println("Part 2 result:", partTwo(lines))
+	fmt.Printf("Part 1 result: %d\n", partOne(lines))
+	fmt.Printf("Part 2 result: %d\n", partTwo(lines))
 }
