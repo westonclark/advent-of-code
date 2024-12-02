948f702bcfbe966c9cfbcf1be04a9a706ba91214
Weston Clark <117543803+westonclark@users.noreply.github.com>
2024-12-02 09:30:04 -0800

    Init go, create template

---
+++ 2024/01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func partOne(lines []string) int {
+	return 0
+}
+
+func partTwo(lines []string) int {
+	return 0
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading input file", err)
+		return
+	}
+
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
---
+++ 2024/go.mod
@@ -0,0 +1,3 @@
+module github.com/westonclark/advent-of-code/2024
+
+go 1.23.3
